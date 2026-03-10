30b4a34e86c25a31f49a777671c2a62da4de3226
Kasper Nilssen <kasper.nilssen1@gmail.com>
2026-03-10 11:19:15 +0100

    feat: implement user authentication schema and update database configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,13 @@
 import { db } from "@/db";
+import * as schema from "@/db/auth-schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema,
   }),
   baseURL: process.env.BETTER_AUTH_URL,
   socialProviders: {
